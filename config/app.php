a739f7166acbc98f65d654fa70447b74e8062cb3
Valera <bikeformer@yandex.ua>
2017-09-28 14:59:43 +0300

    add Debugbar

--- config/app.php
+++ config/app.php
@@ -163,6 +163,13 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+
+        /*
+         * DEV
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -225,6 +232,14 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
+        /*
+         *  DEV
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
