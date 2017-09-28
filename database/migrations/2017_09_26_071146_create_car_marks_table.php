b57073269490a80f5d2cb1076a2f930d6c6a1850
Valera <bikeformer@yandex.ua>
2017-09-28 14:54:53 +0300

    migration

--- database/migrations/2017_09_26_071146_create_car_marks_table.php
+++ database/migrations/2017_09_26_071146_create_car_marks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCarMarksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('car_marks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email', 256)->unique();
-            $table->smallInteger('is_admin')->default(0);
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -31,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('car_marks');
     }
 }
--- database/migrations/2017_09_26_125714_create_car_motorist_table.php
+++ database/migrations/2017_09_26_125714_create_car_motorist_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCarMotoristTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('car_motorist', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email', 256)->unique();
-            $table->smallInteger('is_admin')->default(0);
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('car_id');
+            $table->integer('motorist_id');
         });
     }
 
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('car_motorist');
     }
 }
